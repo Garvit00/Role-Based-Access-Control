7384ae03cc973fc8cb05c1f482c54db682d0fa90
GARVIT <106368889+Garvit00@users.noreply.github.com>
2024-11-28 03:49:59 +0530

    Update UserManagement.js

--- src/componenets/UserManagement.js
+++ src/componenets/UserManagement.js
@@ -15,7 +15,7 @@ const UserManagement = () => {
   const [openSnackbar, setOpenSnackbar] = useState(false);
   const [snackbarMessage, setSnackbarMessage] = useState("");
 
-  const apiUrl = "https://role-based-access-controls-awmd.onrender.com";
+  const apiUrl = "https://server-1rbb.onrender.com";
   useEffect(() => {
     fetch(`${apiUrl}/users`)
       .then((res) => res.json())
